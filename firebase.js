f64d64932af30fc09edba3a48a26ccb6269d3427
vislavathsrinath <vislavathsrinath125@gmail.com>
2024-08-04 15:25:50 -0500

    first commit

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics,  isSupported } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
