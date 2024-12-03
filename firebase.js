5dd347d12b710369340fac159ec7df0851cb9d2c
vislavathsrinath <vislavathsrinath125@gmail.com>
2024-12-03 01:46:45 -0600

    third

--- firebase.js
+++ firebase.js
@@ -1,14 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBXJuIf5DRwxa7VVFMy37XKjjCYDGX4KjA",
   authDomain: "pantry-tracker-bb6f3.firebaseapp.com",
   projectId: "pantry-tracker-bb6f3",
-  storageBucket: "pantry-tracker-bb6f3.appspot.com",
+  storageBucket: "pantry-tracker-bb6f3.firebasestorage.app",
   messagingSenderId: "651025093961",
   appId: "1:651025093961:web:de96acaa070b575b7bb698",
   measurementId: "G-3ZVFPZRCHY"
@@ -16,22 +20,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Define analytics variable
-let analytics;
-
-// Initialize Firebase Analytics only on the client side
-if (typeof window !== 'undefined') {
-  isSupported().then((supported) => {
-    if (supported) {
-      analytics = getAnalytics(app);
-    }
-  });
-}
-
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-export { firestore, auth };
+export default app;
